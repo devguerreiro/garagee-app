68f7340d7e517f3a65bd70dc8f7d4a8a5846b745
Luis <luiscvlh11@gmail.com>
2025-03-31 23:03:03 -0300

    feat: :sparkles: destaca a pagina atual no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import { PropsWithChildren } from "react";
-import { BookUserIcon, CarIcon, MapPinHouseIcon } from "lucide-react";
+
+import { usePathname } from "next/navigation";
 import Link, { LinkProps } from "next/link";
+
+import { BookUserIcon, CarIcon, MapPinHouseIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 
 function FooterItem({
   children,
   ...props
 }: Readonly<PropsWithChildren & LinkProps>) {
+  const pathname = usePathname();
+
   return (
-    <li className="flex-1">
+    <li
+      className={cn(
+        "flex-1",
+        pathname.includes(props.href.toString()) && "text-primary font-bold"
+      )}
+    >
       <Link
         {...props}
         className="flex flex-col justify-center items-center gap-1"
@@ -20,7 +34,7 @@ function FooterItem({
 
 export default function Footer() {
   return (
-    <footer className="w-full h-16 bg-primary text-white sticky bottom-0 left-0 text-xs">
+    <footer className="w-full h-16 bg-card text-muted-foreground sticky bottom-0 left-0 text-xs">
       <nav className="h-full">
         <ul className="h-full flex items-center">
           <FooterItem href="/vagas">
